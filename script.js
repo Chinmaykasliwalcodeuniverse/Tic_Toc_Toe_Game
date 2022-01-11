99965713c8262caef927fff098f7f6adb0fb709f
Chinmaykasliwalcodeuniverse <96693041+Chinmaykasliwalcodeuniverse@users.noreply.github.com>
2022-01-11 21:51:08 +0530

    a

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 console.log("Welcome to Tic Tac Toe")
-let music = new Audio("music.mp3")
 let audioTurn = new Audio("ting.mp3")
-let gameover = new Audio("gameover.mp3")
 let turn = "X"
 let isgameover = false;
 
